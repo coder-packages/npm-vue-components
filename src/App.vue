2e220c9f6e886f007bb86d78bfc1b47d502aaa20
Alexander Manukian <alikmanukian@gmail.com>
2018-12-30 23:07:48 +0400

    initital commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <modal>
+      <div class="panel">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis recusandae dicta repellendus esse. Culpa impedit odio necessitatibus iusto, blanditiis excepturi.
+      </div>
+    </modal>
+
+    <portal-target name="modals"></portal-target>
+  </div>
+</template>
+
+<script>
+import Modal from './components/Modal/Modal'
+export default {
+  components: {
+    Modal
+  }
+}
+</script>
